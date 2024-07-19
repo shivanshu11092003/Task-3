42bdb0182157d24527cac7de28cb70edf8ded7f6
shivanshu11092003 <sshivanshugupta11@gmail.com>
2024-07-19 23:58:36 +0530

    asds

--- learn.js
+++ learn.js
@@ -9,14 +9,22 @@ function addnumber2(...numbers){ //Spread operator
 let result2=addnumber2(12,23,34,-9);
 console.log(result2);
 //Arrow Function
-/* Hoisting: we can't use hoisting in arrow function but we can use it in normal function that are created using keyword function */
+/* Hoisting: we can't use hoisting in arrow function but we can use it in normal function that are created using keyword function
+  normal function can be decalared and call any time 
+  example:  getData(
+             function getData(){
+              function body
+             }
+              but in  Arrow Function first declared and then call.
+
+             */
 /* arguments : we can't use arguments keyword in arrow instead we have to use ..numbers (spread operator) */
 /* arrow function using this keyword pointing to the window so it not recomend to use in local scope   but normal function pointing to the it's outer function  */
 
 
 //higher order function : function which take function as arguments or accepts as arguments 
 
-//callback function: function that is passed as arguments inside another function
+//callback function: higher order function se jis function ko ham call karte hai usee kahte hai call back function
 function multiply(a,b,callback){
     //higher order function
     let result = a*b;
@@ -25,7 +33,7 @@ function multiply(a,b,callback){
     return () => {console.log(result)}
 
 }
-multiply(2,4,function (val) {console.log(val)}) // call back function
+multiply(2,4,function (val) {console.log(val)}) // call back function :: A callback is a function passed as an argument to another function.
 
 //we can also return function from function
 let functioncall =  multiply(9,9,()=>{})
@@ -60,3 +68,26 @@ functioncall();
  // document.getElementById()
  const id = document.getElementById("hii")
 console.log(id.innerText);
+
+/* fetching data from api is asynnchronous task it will return promises */
+
+async function getData (){ // aysn keyword is uesd to define function that task is asynchronous
+    let result = await fetch("https://dummyjson.com/products"); // if function is aysnc the we can ue await keyword to wait the request
+    console.log(await result.json());
+}
+getData();
+
+
+const form = document.getElementById("form");
+const submitter = document.querySelector("button[value=save]");
+const formData = new FormData(form, submitter);
+
+const output = document.getElementById("output");
+
+for (const [key, value] of formData) {
+  output.textContent += `${key}: ${value}\n`;
+}
+
+/* DOM : Document Object Model allow us to manipulate the element style and content because DOM represent document as node and Object 
+that way programming language can interact with web page */
+
